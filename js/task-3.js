08c782718b9e3b28502b2377c9eaa67d1a4d78c5
ClementineLavender <natalibulavina78@gmail.com>
2026-01-11 21:10:50 +0100

    task-3 HW

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+  let contentNumber = parseFloat(content);
+  let paddingNumber = parseFloat(padding);
+  let borderNumber = parseFloat(border);
+  return contentNumber + (paddingNumber + borderNumber) * 2;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
